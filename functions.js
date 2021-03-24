025e38c3d6b3094afbd857553aa7a4142add249b
Miguel Peres <m.peres@gmail.com>
2021-03-24 14:26:34 -0300

    More refactoring

--- functions.js
+++ functions.js
@@ -1,5 +1,18 @@
+//Extracts the ID from an repeating attribure like repeating_specialized-skills_XXXXXXXXXXX_stat:
+const getUID = function (value) {
+  const regexp = /-.{19}(?=_)|-.{19}$/g;
+  const valueAsString = String(value);
+  const match = valueAsString.match(regexp);
+  return match ? match.join() : "";
+};
+
+const isEventPlayerOriginated = event => {
+  return (event.sourceType && event.sourceType === 'player')
+};
+
 const safeguardArray = value => {
-  return (Array.isArray(value)) ? value : [value];
+  if(value) return (Array.isArray(value)) ? value : [value];
+  return [];
 };
 
 const safeguardUniqueArray = value => {
@@ -19,7 +32,7 @@ const getSkillRelatedAttributes = skills => {
   skills = safeguardArray(skills);
   const skillRelatedAttributes = ['stat', 'rank', 'total'];
   
-  const attributes = [];
+  let attributes = [];
   skills.forEach(skill => {
     skillRelatedAttributes.forEach(attribute => {
       attributes.push(`${skill}_${attribute}`);
@@ -38,27 +51,52 @@ const updateGeneralSkills = (skills, callback) => {
   skillRelatedAttributes = getSkillRelatedAttributes(skills);
 
   getAttrs(skillRelatedAttributes, values => {
-    const stat = getNumericValue(values[statAttribute]);
-    const rank = getNumericValue(values[rankAttribute]);
-    const total = stat + rank;
-    const update = {
-      [totalAttribute]: total,
-    };
-    setAttrs(update);
-
     skills.forEach(skill => {
       const update = {};
 
-      //Calculate Rank
+      //Calculate stat
       const rank = safeguardInteger(values[`${skill}_rank`]);
       const relatedStats = globalAttributesbyCategory.generalSkills[skill];
       const statA = safeguardInteger(values[relatedStats[0]]);
       const statB = safeguardInteger(values[relatedStats[1]]);
       const stat = (rank > 0) ? Math.max(statA, statB) : Math.min(statA, statB);
       update[`${skill}_stat`] = stat;
-      
+
+      //Calculate total
+      const total = stat + rank;
+      update[`${skill}_total`] = total;
+
       setAttrs(update, callback);
     });
+  });
+};
 
-  }, callback);
+const processSpecializedSkills = uids => {
+  let relatedAttributes = globalAttributesbyCategory.stats;
+  const prefix = 'repeating_specialized-skills';
+  uids.forEach(uid => {
+    relatedAttributes.push(`${prefix}_${uid}_name`);
+    relatedAttributes.push(`${prefix}_${uid}_stat`);
+    relatedAttributes.push(`${prefix}_${uid}_rank`);
+  });
+  getAttrs(relatedAttributes, values => {
+    const update = {};
+    uids.forEach(uid => {
+      const stat = safeguardInteger(values[values[`${prefix}_${uid}_stat`]]);
+      const rank = safeguardInteger(values[`${prefix}_${uid}_rank`]);
+      const total = stat + rank;
+      update[`${prefix}_${uid}_total`] = total;
+    });
+    setAttrs(update);
+  });
+};
+const updateSpecializedSkills = uids => {
+  uids = safeguardArray(uids);
+  if(uids.length === 0) {
+    getSectionIDs('specialized-skills', ids => {
+      processSpecializedSkills(ids)
+    });
+  } else {
+    processSpecializedSkills(uids);
+  }
 };
--- listeners.js
+++ listeners.js
@@ -1,152 +1,9 @@
-const globalAttributesbyCategory = {
-  stats: [
-    "strength",
-    "agility",
-    "endurance",
-    "intelligence",
-    "perception",
-    "wisdom",
-    "spirit",
-    "charisma",
-    "will",
-  ],
-  generalSkills: {
-    art: ["spirit", "will"],
-    atheletics: ["strength", "agility"],
-    ceremony: ["wisdom", "spirit"],
-    charm: ["charisma", "perception"],
-    coercion: ["charisma", "spirit"],
-    computers: ["intelligence", "wisdom"],
-    crafting: ["perception", "spirit"],
-    cybernetics: ["intelligence", "wisdom"],
-    deception: ["charisma", "will"],
-    farming: ["intelligence", "endurance"],
-    "first-aid": ["intelligence", "will"],
-    herbalism: ["perception", "wisdom"],
-    husbandry: ["charisma", "wisdom"],
-    investigation: ["perception", "wisdom"],
-    knowledge: ["intelligence", "wisdom"],
-    language: ["perception", "wisdom"],
-    medicine: ["intelligence", "wisdom"],
-    "melee-weapons": ["strength", "endurance"],
-    music: ["spirit", "perception"],
-    performance: ["charisma", "spirit"],
-    piloting: ["intelligence", "agility"],
-    "ranged-weapons": ["perception", "agility"],
-    science: ["intelligence", "perception"],
-    skulduggery: ["perception", "charisma"],
-    stealth: ["agility", "wisdom"],
-    survival: ["endurance", "wisdom"],
-    tracking: ["perception", "wisdom"],
-    "unarmed-combat": ["strength", "intelligence"],
-  },
-  skillsByAttribute: function () {
-    const list = {};
-    globalAttributesbyCategory.stats.forEach(stat => {
-      list[stat] = Object.keys(globalAttributesbyCategory.generalSkills).filter(
-        skill => {
-          return (
-            globalAttributesbyCategory.generalSkills[skill].indexOf(stat) > -1
-          );
-        }
-      );
-    });
-    return list;
-  },
-};
-
-//Extracts the ID from an repeating attribure like repeating_specialized-skills_XXXXXXXXXXX_stat:
-const getUID = function (value) {
-  const regexp = /-.{19}(?=_)|-.{19}$/g;
-  const valueAsString = String(value);
-  const match = valueAsString.match(regexp);
-  return match ? match.join() : "";
-};
-
-//Reads text attribute and return its value or 0 when not valid:
-const getNumericValue = function (attribute, defaultValue = 0) {
-  return attribute && !isNaN(attribute) ? parseInt(attribute) : defaultValue;
-};
-
-//Reads the stat and rank o the chosen skill and updates its total:
-const updateSkillTotal = function (
-  statAttribute,
-  rankAttribute,
-  totalAttribute
-) {
-  getAttrs([statAttribute, rankAttribute], values => {
-    const stat = getNumericValue(values[statAttribute]);
-    const rank = getNumericValue(values[rankAttribute]);
-    const total = stat + rank;
-    const update = {
-      [totalAttribute]: total,
-    };
-    setAttrs(update);
-  });
-};
-
-// Sets a listerner to update the skill total on changes to skills stats and ranks:
+// Sets a listerner to update a skill when its rank changes
 Object.keys(globalAttributesbyCategory.generalSkills).forEach(skill => {
-  on(`change:${skill}_stat change:${skill}_rank`, eventinfo => {
-    if (!eventinfo.sourceType || eventinfo.sourceType !== "player") {
-      return;
-    }
-
-    updateSkillTotal(`${skill}_stat`, `${skill}_rank`, `${skill}_total`);
-    console.log("Updating General Skills Totals");
-  });
-});
-
-//Update a list of skills related to a specific attribure (Strength, Agility, ...):
-const updateSkillsStats = function (skillList, callback = () => {}) {
-  const skillAttributes = skillList.map(skill => {
-    return `${skill}_rank`;
-  });
-
-  let statAttributes = [];
-  skillList.forEach(skill => {
-    statAttributes = statAttributes.concat(
-      globalAttributesbyCategory.generalSkills[skill]
-    );
-  });
-
-  const filteredStatAttributes = Array.from(new Set(statAttributes));
-  getAttrs([...skillAttributes, ...filteredStatAttributes], values => {
-    skillList.forEach(skill => {
-      if (!isNaN(values[`${skill}_rank`])) {
-        const relatedStats = globalAttributesbyCategory.generalSkills[skill];
-        const statA = getNumericValue(values[relatedStats[0]]);
-        const statB = getNumericValue(values[relatedStats[1]]);
-        const stat =
-          parseInt(values[`${skill}_rank`]) > 0
-            ? Math.max(statA, statB)
-            : Math.min(statA, statB);
-        const update = {
-          [`${skill}_stat`]: stat,
-        };
-        setAttrs(update, callback);
-      }
-    });
-  });
-};
-
-//Sets a listerner to update skill stats on changes to linked stat:
-const skillsByAttributes = globalAttributesbyCategory.skillsByAttribute();
-Object.keys(skillsByAttributes).forEach(attribute => {
-  on(`change:${attribute}`, eventinfo => {
-    updateSkillsStats(skillsByAttributes[attribute]);
-    console.log("Updating Skill Stat");
-  });
-});
-
-// When a rank is changed, update stat:
-
-Object.keys(globalAttributesbyCategory.generalSkills).forEach(skill => {
-  on(`change:${skill}_rank`, eventinfo => {
-    // const newRank = getNumericValue(eventinfo.newValue)
-    updateSkillsStats([skill], () => {
-      updateSkillTotal(`${skill}_stat`, `${skill}_rank`, `${skill}_total`);
-    });
+  on(`change:${skill}_rank`, event => {
+    if(!isEventPlayerOriginated(event)) return;
+    updateGeneralSkills(skill);
+    console.log(`Updating General Skill: ${skill}`);
   });
 });
 
@@ -155,10 +12,21 @@ on(
   "change:repeating_specialized-skills:stat change:repeating_specialized-skills:rank",
   eventinfo => {
     const uid = getUID(eventinfo.sourceAttribute);
-    const prefix = `repeating_specialized-skills_${uid}`;
-    updateSkillTotal(`${prefix}_stat`, `${prefix}_rank`, `${prefix}_total`);
-    console.log("Updating Specialized Skills Totals");
+    updateSpecializedSkills(uid);
+    console.log(`Updating General Skill: ${uid}`);
   }
 );
+
+//Sets a listerner to update skills that a linked to a specific stats
+const skillsByAttributes = globalAttributesbyCategory.skillsByAttribute();
+Object.keys(skillsByAttributes).forEach(attribute => {
+  on(`change:${attribute}`, eventinfo => {
+    const skills = skillsByAttributes[attribute];
+    updateGeneralSkills(skills);
+    updateSpecializedSkills();
+    console.log(`Updating General Skill: ${skills.join(', ')}`);
+    console.log(`Updating All Specialized Skills`);
+  });
+});
 
 //# sourceURL=coyoteandcrow.js
